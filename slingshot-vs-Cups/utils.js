fd9ee949df40aadc634c7dc9f22ab73435478e28
Szymon Zych <szymonzych868@gmail.com>
2025-08-31 21:01:39 +0200

    Add utility functions for random number generation and shape drawing

---
+++ slingshot-vs-Cups/utils.js
@@ -0,0 +1,19 @@
+export function rand(min, max) {
+  return Math.random() * (max - min) + min;
+}
+export function roundRect(ctx, x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.arcTo(x + w, y, x + w, y + h, r);
+  ctx.arcTo(x + w, y + h, x, y + h, r);
+  ctx.arcTo(x, y + h, x, y, r);
+  ctx.arcTo(x, y, x + w, y, r);
+  ctx.closePath();
+}
+export function circleRectOverlap(cx, cy, cr, rx, ry, rw, rh) {
+  const nx = Math.max(rx, Math.min(cx, rx + rw));
+  const ny = Math.max(ry, Math.min(cy, ry + rh));
+  const dx = cx - nx,
+    dy = cy - ny;
+  return dx * dx + dy * dy <= cr * cr;
+}
