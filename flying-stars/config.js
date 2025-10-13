264af1754acd4f424e757eb0643dd4e74c0e6274
Szymon Zych <szymonzych868@gmail.com>
2025-10-13 22:25:47 +0200

    Add configuration file for flying stars animation settings

---
+++ flying-stars/config.js
@@ -0,0 +1,42 @@
+export const config = {
+  colors: [
+    "#FFD700",
+    "#FF69B4",
+    "#00CED1",
+    "#FF1493",
+    "#9370DB",
+    "#00FF00",
+    "#FF4500",
+    "#4169E1",
+    "#FFD700",
+    "#FF00FF",
+    "#00FFFF",
+    "#FFFF00",
+  ],
+
+  numberOfStars: 100,
+  mouseRadius: 150,
+
+  speedY: {
+    min: 0.5,
+    max: 2.0,
+  },
+  speedX: {
+    min: -0.25,
+    max: 0.25,
+  },
+
+  size: {
+    min: 2,
+    max: 5,
+  },
+
+  opacity: {
+    min: 0.5,
+    max: 1.0,
+  },
+
+  repulsionForce: 5,
+
+  shadowBlur: 15,
+};
