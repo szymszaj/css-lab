1e81b3ffc73a9525c8be63d0514492e5999aeb77
Szymon Zych <szymonzych868@gmail.com>
2026-01-09 19:23:09 +0100

    fix error build

--- scroll-cards-animation/script.js
+++ scroll-cards-animation/script.js
@@ -39,7 +39,6 @@ class CyberInterface {
         if (entry.isIntersecting) {
           entry.target.classList.add("visible");
 
-          
           const title = entry.target.querySelector(".card-content h2");
           if (title && !title.dataset.scrambled) {
             this.scrambleText(title);
@@ -60,26 +59,24 @@ class CyberInterface {
         const rect = card.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-        
+
         const centerX = rect.width / 2;
         const centerY = rect.height / 2;
 
-        const rotateX = ((y - centerY) / centerY) * -10; to 10 deg
+        const rotateX = ((y - centerY) / centerY) * -10;
         const rotateY = ((x - centerX) / centerX) * 10;
 
         card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
       });
 
       card.addEventListener("mouseleave", () => {
-        
         card.style.transform =
           "perspective(1000px) rotateX(0) rotateY(0) scale(1)";
       });
     });
   }
 
-  setupTextScramble() {
-  }
+  setupTextScramble() {}
   scrambleText(element) {
     const originalText = element.innerText;
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*";
@@ -100,7 +97,7 @@ class CyberInterface {
         clearInterval(interval);
       }
 
-      iterations += 1 / 2; 
+      iterations += 1 / 2;
     }, 30);
   }
 }
